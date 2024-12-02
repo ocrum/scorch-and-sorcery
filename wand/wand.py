63f93464966eaa4bbcab1e929e4b43f1047d2bf0
Daniel B <danielbarsc@gmail.com>
2024-12-02 17:24:51 -0500

    removed tag code from wang

--- wand/wand.py
+++ wand/wand.py
@@ -3,12 +3,6 @@ from machine import Pin, SoftI2C, ADC, PWM
 import asyncio
 from lsm6ds3 import LSM6DS3
 from button import Button
-from led import LED
-
-class GameMode:
-    S_and_S = "scorch & sorcery"
-    INFERNO = "inferno chase"
-    PUZZLE = "puzzle"
 
 class Spell:
     UP = "up"
@@ -20,16 +14,11 @@ class Spell:
 class Wand:
     def __init__(self):
         self.tag_state = False     # whether tagged
-        self.game_mode = GameMode.S_and_S
         self.msg = ""
 
         # Initialize I2C
-        self.i2c = SoftI2C(scl=Pin(7), sda=Pin(6))
+        self.i2c = SoftI2C(scl=Pin(23), sda=Pin(22)) # 23 -> Pin 5; 22 -> Pin 4
         self.lsm = LSM6DS3(self.i2c)
-
-        self.red = LED(pin_num=0)
-        self.green = LED(pin_num=1)
-        self.white = LED(pin_num=2)
 
         self.button = Button(pin_num=9)
 
@@ -39,16 +28,6 @@ class Wand:
     def my_callback(self):
         for mac, message, rtime in self.networking.aen.return_messages(): #You can directly iterate over the function
             self.msg = message
-
-    async def tag(self):
-        # If in proximity to dragon - rssi:
-        self.tag_state = True
-        # LED turns red
-        self.red.on()
-        self.green.off()
-        self.white.off()
-
-        # self.networking.send(b'\xFF\xFF\xFF\xFF\xFF\xFF', b'!tagged')
 
     def read_movement_data(self):
         ax, ay, az, gx, gy, gz = self.lsm.get_readings()
@@ -68,7 +47,7 @@ class Wand:
         return max_spell if max_count > 0 else Spell.OTHER
 
     async def puzzle(self):
-        if self.game_mode != GameMode.INFERNO and self.button.is_pressed():
+        if self.button.is_pressed():
             lr_data = []
             ud_data = []
             while self.button.is_being_pressed():
@@ -84,21 +63,13 @@ class Wand:
             else:
                 print(movement)
                 self.networking.aen.send(b'\xFF\xFF\xFF\xFF\xFF\xFF', b'!' + movement)
-                await asyncio.sleep_ms(10)  # Tune the cooldown as needed
-
-    async def win_check(self):
-        if self.msg == "!completed":
-            self.red.off()
-            self.green.on()
-            self.white.off()
+                await asyncio.sleep_ms(10)  # TODO Tune the cooldown as needed
 
     async def run(self):
         while True:
             self.my_callback()
             await asyncio.gather(
-                self.win_check(),
                 self.puzzle(),
-                self.tag()
             )
 
 # Initialize the Wand object and run the tasks asynchronously
